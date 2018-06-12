b4bf97734eae088953ffd862393db27c14e4ed60
Anthony Lenglet <Anthony.Lenglet@hotmail.com>
2018-06-12 13:44:22 +0200

    Add: flatpickr

--- src/lib/flatpickr.component.ts
+++ src/lib/flatpickr.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import flatpickr from "flatpickr";
+
 @Component({
   selector: 'ngx-flatpickr',
   template: `
-    <p>
-      flatpickr works!
-    </p>
+    <div #flatpickr></div>
   `,
   styles: []
 })
 export class FlatpickrComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('flatpickr') private el: ElementRef
+  private options: object;
+
+  constructor() {
+    this.options = {
+    };
+  }
 
   ngOnInit() {
+    flatpickr(this.el.nativeElement, options);
   }
 
 }
