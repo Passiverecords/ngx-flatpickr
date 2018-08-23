b97a9c226bcbf297ed8cb8d0e7e309180ebfe593
Anthony Lenglet <Anthony.Lenglet@hotmail.com>
2018-08-23 12:12:30 +0200

    Change: removed the stylesheet link from the template

--- src/lib/ngxflatpickr.component.ts
+++ src/lib/ngxflatpickr.component.ts
@@ -1,35 +1,45 @@
-import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
-import flatpickrImport from 'flatpickr';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewEncapsulation
+} from '@angular/core'
+import flatpickrImport from 'flatpickr'
 
 @Component({
   selector: 'ngx-flatpickr',
   template: `
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <input #container [class]="class" [ngModel]="value" (ngModelChange)="change($event)">
   `,
-  styles: []
+  styleUrls: [
+    "https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class NgxFlatpickrComponent implements OnInit {
 
-  @ViewChild('container') private el: ElementRef;
-  @Input() private options: object = {};
-  private pickerObj: object;
+  @ViewChild('container') private el: ElementRef
+  @Input() private options: object = {}
 
-  @Input() public class: string;
+  @Input() public class: string
 
-  @Input() public value: string;
-  @Output() public valueChange = new EventEmitter();
+  @Input() public value: string
+  @Output() public valueChange = new EventEmitter()
 
   constructor() {
   }
 
   ngOnInit(): void {
-    flatpickrImport(this.el.nativeElement, this.options);
+    flatpickrImport(this.el.nativeElement, this.options)
   }
 
   change(newValue) {
-    this.value = newValue;
-    this.valueChange.emit(newValue);
+    this.value = newValue
+    this.valueChange.emit(newValue)
   }
 
 }
